66b401941c5022c99f888a85062560f2e658c5bf
YagoLopolito <yagolopolito@gmail.com>
2022-05-03 10:30:12 -0300

    data_bases

---
+++ DeleteCar.sql
@@ -0,0 +1,18 @@
+delete from Cars where idcar = 001;
+
+delete from Cars where idcar = 004;
+
+delete from Cars where idcar = 005;
+
+delete from Clients where idClient = 002;
+
+delete from Clients where idClient = 003;
+
+delete from Clients where firstName = "Federico";
+
+delete from Clients where firstName = "papel";
+
+
+
+insert into Clients
+values(001, "Yago", "Lopolito", true, false)
---
+++ GroupBy.sql
@@ -0,0 +1,21 @@
+select maxSpeed
+from Cars
+GROUP BY maxSpeed;
+
+select idCar
+from Cars
+GROUP BY idCar;
+
+select firstName
+from Clients
+GROUP BY firstName;
+
+select distance
+from Invoices
+GROUP BY distance;
+
+select distance, estimatedTimeOfArrival
+from Invoices
+GROUP BY distance, estimatedTimeOfArrival;
+
+
